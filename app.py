41432ffc8383cd698651333dac700dbcf7274f9c
SergSkm <serg.skm@yandex.ru>
2019-12-23 11:34:42 +0300

    proper first version

---
+++ app.py
@@ -0,0 +1,8 @@
+# run.py
+
+from app import app
+from waitress import serve
+
+if __name__ == '__main__':
+    #app.run(threaded=True, debug=True)   # unix based for gunicorn
+    serve(app, host='0.0.0.0', port=80)
